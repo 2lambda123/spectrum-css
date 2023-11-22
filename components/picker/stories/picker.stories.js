365d7a9e0e5f00c47e87eadb8c1998e129b33db9
Jenn Diaz <jenn.diaz@heysparkbox.com>
2023-11-22 10:13:59 -0700

    Revert "feat(picker): add quiet side label to story"
    This reverts commit 89a4ffd06d4d48d0413e61dd0cd9d322fb2255b7.

--- components/picker/stories/picker.stories.js
+++ components/picker/stories/picker.stories.js
@@ -157,15 +157,6 @@ Quiet.args = {
 	],
 };
 
-export const QuietSideLabel = Template.bind({});
-QuietSideLabel.args = {
-	isQuiet: true,
-	content: [
-		() => MenuStories(MenuStories.args)
-	],
-	labelPosition: "left"
-};
-
 export const Loading = Template.bind({});
 Loading.args = {
 	isLoading: true,
